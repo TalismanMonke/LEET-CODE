e985c2d832f8af13ace8fed90b0478e9c3534ac7
Atanu the great <138390460+TalismanMonke@users.noreply.github.com>
2026-05-06 15:23:55 +0530

    Time: 0 ms (100%), Space: 44.1 MB (55.61%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,23 +1,14 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int left = 0, right = nums.length - 1;
-
-        // Use left < right to find the convergence point
         while (left < right) {
             int mid = left + (right - left) / 2;
-
-            // Are we on an upward slope?
             if (nums[mid] < nums[mid + 1]) {
-                // Yes, so the peak is to the right
                 left = mid + 1;
             } else {
-                // No, we are on a downward slope or at a peak
-                // The peak is at mid or to the left
                 right = mid;
             }
         }
-        
-        // When left == right, they point to a peak element
         return left;
     }
 }
